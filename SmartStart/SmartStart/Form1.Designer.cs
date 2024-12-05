4fee97d1017f05b2db3c248328266ddb2521470e
maya-wyganowska <mayawyga24@gmail.com>
2024-12-04 19:16:56 -0600

    Add files via upload

---
+++ SmartStart/SmartStart/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace SmartStart
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSmartStart = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblSmartStart
+            // 
+            this.lblSmartStart.AutoSize = true;
+            this.lblSmartStart.Font = new System.Drawing.Font("Modern No. 20", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSmartStart.Location = new System.Drawing.Point(281, 40);
+            this.lblSmartStart.Name = "lblSmartStart";
+            this.lblSmartStart.Size = new System.Drawing.Size(206, 41);
+            this.lblSmartStart.TabIndex = 0;
+            this.lblSmartStart.Text = "Smart Start";
+            this.lblSmartStart.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblSmartStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSmartStart;
+    }
+}
+
